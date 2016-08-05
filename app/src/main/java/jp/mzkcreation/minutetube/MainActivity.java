75d721117cdef07359e4d4543c8b481ab6a2b7f4
MizukiNakano <mizuki590@gmail.com>
2016-08-05 13:00:44 +0900

    youtube data api test

--- app/src/main/java/jp/mzkcreation/minutetube/MainActivity.java
+++ app/src/main/java/jp/mzkcreation/minutetube/MainActivity.java
@@ -1,15 +1,43 @@
 package jp.mzkcreation.minutetube;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
+    /** Global instance properties filename. */
+    private static String PROPERTIES_FILENAME = "youtube.properties";
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    private static YouTube youtube;
+    /** Global instance of the max number of videos we want returned (50 = upper limit per page). */
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+    SearchTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        task = new SearchTask();
+        task.execute("word");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +81,77 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+
+        System.out.println("\n=============================================================");
+        System.out.println(
+                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
+        System.out.println("=============================================================\n");
+
+        if (!iteratorSearchResults.hasNext()) {
+            System.out.println(" There aren't any results for your query.");
+        }
+
+        while (iteratorSearchResults.hasNext()) {
+
+            SearchResult singleVideo = iteratorSearchResults.next();
+            ResourceId rId = singleVideo.getId();
+
+            // Double checks the kind is video.
+            if (rId.getKind().equals("youtube#video")) {
+                Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
+
+                System.out.println(" Video Id" + rId.getVideoId());
+                System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
+                System.out.println(" Thumbnail: " + thumbnail.getUrl());
+                System.out.println("\n-------------------------------------------------------------\n");
+            }
+        }
+    }
+
+    class SearchTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... words){
+            String query = "a";
+
+            Properties properties = new Properties();
+            try {
+                InputStream in = MainActivity.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+                properties.load(in);
+            } catch (IOException e) {
+                System.err.println("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause()
+                        + " : " + e.getMessage());
+                System.exit(1);
+            }
+
+            try {
+                youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).setApplicationName("youtube-cmdline-search-sample").build();
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+                String apiKey = properties.getProperty("youtube.apikey");
+                search.setKey(apiKey);
+                search.setQ(query);
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                SearchListResponse searchResponse = search.execute();
+                List<SearchResult> searchResultList = searchResponse.getItems();
+                if (searchResultList != null) {
+                    prettyPrint(searchResultList.iterator(), query);
+                }
+
+            } catch (IOException e){
+
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+
+        }
+    }
 }
+
