7f412f136cce84a0e3587f6f8406182767ee0b71
MizukiNakano <mizuki590@gmail.com>
2016-08-10 17:34:42 +0900

    use youtube player instead of fragment

--- app/src/main/java/jp/mzkcreation/minutetube/PlayerActivity.java
+++ app/src/main/java/jp/mzkcreation/minutetube/PlayerActivity.java
@@ -1,18 +1,25 @@
 package jp.mzkcreation.minutetube;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
-public class PlayerActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener{
+public class PlayerActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener, YouTubePlayer.OnFullscreenListener{
+
+    YouTubePlayerView youtubeView;
+    LinearLayout baseLayout;
     String videoId;
+    boolean isFullScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +35,11 @@ public class PlayerActivity extends AppCompatActivity implements YouTubePlayer.O
         TextView dv = (TextView) findViewById(R.id.player_description);
         dv.setText(description);
 
-
-        // フラグメントインスタンスを取得
-        YouTubePlayerFragment youTubePlayerFragment =
-                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtubefragment);
-
         String key = Util.getYoutubeProperty().getProperty("youtube.apikey");
-        // フラグメントのプレーヤーを初期化する
-        youTubePlayerFragment.initialize(key, this);
+        youtubeView = (YouTubePlayerView) findViewById (R.id.youtube_view);
+        youtubeView.initialize(key, this);
+
+        baseLayout = (LinearLayout) findViewById(R.id.player_base);
     }
 
     @Override
@@ -43,11 +47,68 @@ public class PlayerActivity extends AppCompatActivity implements YouTubePlayer.O
         Log.d("warn", "Initialization Failure");
     }
 
+
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
-        if (!wasRestored) {
-            // 指定された動画のサムネイルを読み込み、プレーヤーがその動画を再生する準備を行う
-            player.cueVideo(videoId);
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, final YouTubePlayer player, boolean wasRestored) {
+        // Playerフルスクリーン時の挙動を手動制御に設定
+        player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        player.setOnFullscreenListener(this);
+        if (!wasRestored){
+            player.loadVideo(videoId);
+        }
+    }
+
+    @Override
+    public void onFullscreen(boolean isFullscreen) {
+        this.isFullScreen = isFullscreen;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // 横向きにした場合にアクティビティが再構成されないようにする必要がある。
+        doLayout();
+    }
+
+    private void doLayout() {
+        // プレイヤーのパラメータを取得
+        LinearLayout.LayoutParams playerParams = (LinearLayout.LayoutParams) youtubeView.getLayoutParams();
+        // フルスクリーンの場合
+        if (isFullScreen) {
+            // YouTubePlayerの画面サイズを設定
+            playerParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            playerParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+            // プレイヤー以外を非表示にする
+//            toolbar.setVisibility(View.GONE);
+//            otherViews.setVisibility(View.GONE);
+        }
+        // フルスクリーンではない場合
+        else {
+            // プレイヤー以外も表示する
+//            toolbar.setVisibility(View.VISIBLE);
+//            otherViews.setVisibility(View.VISIBLE);
+            // 画面を横向きにした場合のレイアウト
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // PlayerとotherViewsのパラメータを設定
+                playerParams.width = 0;
+//                otherViewsParams.width = 0:
+                playerParams.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//                otherViewsParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+                playerParams.weight = 1;
+                // 外枠のレイアウト
+                baseLayout.setOrientation(LinearLayout.HORIZONTAL);
+            }
+            // 画面が縦の場合のレイアウト
+            else {
+                // PlayerとotherViewsのパラメータを設定
+                playerParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+//                otherViewsParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+                playerParams.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                playerParams.weight = 0;
+//                otherViewsParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+                // 外枠のレイアウト
+                baseLayout.setOrientation(LinearLayout.VERTICAL);
+            }
         }
     }
 }
